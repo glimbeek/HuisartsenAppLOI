52131f48fb895ca51ee291aad7902de874c4ac1b
glimbeek <gvanlimbeek@hotmail.com>
2018-04-12 08:57:20 +0200

    fixed missing viewCtrl reference

--- src/pages/camera-modal/camera-modal.ts
+++ src/pages/camera-modal/camera-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
 import { StorageProvider } from '../../providers/storage/storage';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
@@ -23,6 +23,7 @@ export class CameraModalPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public storageProvider: StorageProvider,
+              public viewCtrl: ViewController,
               private camera: Camera) {
   }
 
