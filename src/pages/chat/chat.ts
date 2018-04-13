026a3058795e592e52048003dac44406e722a57c
glimbeek <gvanlimbeek@hotmail.com>
2018-04-13 14:40:41 +0200

    fix for chat page camera call

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -49,12 +49,22 @@ export class ChatPage {
 
   doLoadCamera() {
     console.log('Started the camera.');
+
+
     const options: CameraOptions = {
       quality: 70,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
-    }    
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
